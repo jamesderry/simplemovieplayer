d0545bb3069aa31c5f94fe492e3a195e0629081b
James Derry <jderry@derryitconsulting.com>
2013-11-25 19:52:56 -0600

    Initial Commit

---
+++ iOS3Lab6x/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS3Lab6x
+//
+//  Created by James Derry on 11/25/13.
+//  Copyright (c) 2013 James Derry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS3Lab6x/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS3Lab6x
+//
+//  Created by James Derry on 11/25/13.
+//  Copyright (c) 2013 James Derry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
